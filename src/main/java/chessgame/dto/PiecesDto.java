3112d2e82c5a0d342a19872900d8408c68e3aae7
fram1998 <koust6u@pusan.ac.kr>
2024-04-01 14:52:31 +0900

    fix(All): 미사용 클래스 삭제

--- src/main/java/chessgame/dto/PiecesDto.java
+++
@@ -1,7 +0,0 @@
-package chessgame.dto;
-
-import java.util.List;
-
-public record PiecesDto(List<PieceDto> pieceDtos) {
-
-}
--- src/main/java/chessgame/dto/PointDto.java
+++
@@ -1,5 +0,0 @@
-package chessgame.dto;
-
-public record PointDto(int horizontalPoint, int verticalPoint) {
-
-}
